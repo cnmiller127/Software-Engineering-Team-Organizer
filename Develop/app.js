426fd79e53de17ad16abdaf2e366c8253e0d81f0
Christian Miller <cnmiller127@gmail.com>
2020-07-14 10:30:07 -0500

    phase 4

--- Develop/app.js
+++ Develop/app.js
@@ -1,23 +1,55 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
 const { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } = require("constants");
 const { lookup } = require("dns");
+var idBad = false;
 var teamArr = [];
+var idArr = [];
+
+async function introMsg(){
+   const yo =  {name: "intro", message: "Welcome to the Sofware Development Team Organizer! Please press enter and follow the directions."};
+   const {intro} = await inquirer.prompt(yo);
+   managerGen();
+   
+}
+
+const isValidId = async (number) => {
+    idBad = false;
+    idArr.forEach(id => {
+        if(number === id)
+        {
+            idBad = true;
+        }
+        
+    });
+
+    if(isNaN(number)){
+        return "This is not a valid number. Please enter a valid number.";
+    }
+    else if(idBad === true){
+        return "This id was already used! Please enter a new number.";
+    }
+
+    else{
+        return true;
+    }
+};
+const validOfficeNum = async (number) => {
+    return (isNaN(number) ? "This is not a valid number. Please enter a valid number.": true);
+}
 // Questions specific to the manager
 const mgrQuestions = [{type: "input", name: "name", message: "Enter Manager name: "},
-{type: "input", name: "id", message: "Enter Manager ID #: "},
-{type: "input", name: "email", message: "Enter Manger e-mail address: "},
-{type: "input", name: "officeNumber", message: "Enter Manager office number: "}];
+{type: "number", name: "id", message: "Enter Manager ID #: ", validate: isValidId},
+{type: "input", name: "email", message: "Enter Manager e-mail address: "},
+{type: "number", name: "officeNumber", message: "Enter Manager office number: ", validate: validOfficeNum}];
 
 //Function specific to the manager
 async function managerGen(){
@@ -25,6 +57,7 @@ async function managerGen(){
     const {name, id, email, officeNumber, roll} = await inquirer.prompt(mgrQuestions); 
     const manager = new Manager(name, id, email, officeNumber);
     teamArr.push(manager);
+    idArr.push(id);
     engOrInt();
     }
     catch{
@@ -32,8 +65,7 @@ async function managerGen(){
     }
 
 }
-
-managerGen();
+introMsg();
 
 //Question to determine roll of next employee, this should be iterated until user opts out
 const rollQ = [{type: "list", name: "roll", message: "Select  employee roll: ", choices: ["Engineer", "Intern"]}];
@@ -49,7 +81,7 @@ async function engOrInt(){
 }
 //Questions for engineer 
 const engQs = [{type: "input", name: "name", message: "Enter Engineer's name: "},
-{type: "input", name: "id", message: "Enter Engineer's ID #: "},
+{type: "number", name: "id", message: "Enter Engineer's ID #: ", validate: isValidId},
 {type: "input", name: "email", message: "Enter Engineer's e-mail address: "},
 {type: "input", name: "Github", message: "Enter Engineer's GitHub profile URL: "}];
 //Function for generating engineer card
@@ -57,11 +89,12 @@ async function engineerGen(){
     const {name, id, email, Github} = await inquirer.prompt(engQs);
     const engineer = new Engineer(name, id, email, Github);
     teamArr.push(engineer);
+    idArr.push(id);
     isDone();
 }
 //Questions for Intern 
 const internQs = [{type: "input", name: "name", message: "Enter Intern's name: "},
-{type: "input", name: "id", message: "Enter Intern's ID #: "},
+{type: "number", name: "id", message: "Enter Intern's ID #: ", validate: isValidId},
 {type: "input", name: "email", message: "Enter Intern's e-mail address: "},
 {type: "input", name: "school", message: "Enter Intern's school name: "}];
 //Function for generating Intern card
@@ -69,6 +102,7 @@ async function internGen(){
     const {name, id, email, school} = await inquirer.prompt(internQs);
     const intern = new Intern(name, id, email, school);
     teamArr.push(intern);
+    idArr.push(id);
     isDone();
 }
 
@@ -88,7 +122,6 @@ async function isDone(){
 async function genHTML(){
     try{
         const html = await render(teamArr);
-        console.log(outputPath);
         fs.writeFileSync(outputPath, html);
         //window.open(outputPath);
     }
@@ -96,6 +129,10 @@ async function genHTML(){
         console.log("error");
     }
 }
+const validIdQ = [{type: "number", name: "id", message: "This ID has been selected or is invalid. Please enter a valid ID: "}]
+
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
